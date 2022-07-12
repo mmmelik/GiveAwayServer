efe078b1eab54838b4f5d3d6a83f1d1e932129ff
Kadir Özyıldırım <kadirozyildirim@Kadirs-MacBook-Pro.local>
2022-07-12 19:18:39 +0300

    Initial commit

---
+++ src/main/java/app/freegiveaway/server/FreeGiveAwayServerApplication.java
@@ -0,0 +1,13 @@
+package app.freegiveaway.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeGiveAwayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeGiveAwayServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/freegiveaway/server/FreeGiveAwayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.freegiveaway.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeGiveAwayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
